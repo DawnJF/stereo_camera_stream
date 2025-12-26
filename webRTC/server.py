2b9fb66b894a42ddb83387217a296e86a0c4831a
MJF <majianfei1001@gmail.com>
2025-12-26 16:22:11 +0800

    add ar mode

--- webRTC/server.py
+++ webRTC/server.py
@@ -78,6 +78,12 @@ async def babylon(request):
     return web.Response(content_type="text/html", text=content)
 
 
+async def seethrough(request):
+    with open(os.path.join(ROOT, "seethrough.html"), "r", encoding="utf-8") as f:
+        content = f.read()
+    return web.Response(content_type="text/html", text=content)
+
+
 async def offer(request):
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
@@ -169,7 +175,8 @@ if __name__ == "__main__":
     app = web.Application()
     app.on_shutdown.append(on_shutdown)
     app.router.add_get("/", index)
-    app.router.add_get("/babylon", babylon)
+    app.router.add_get("/video", babylon)
+    app.router.add_get("/ar", seethrough)
     app.router.add_post("/offer", offer)
 
     if args.cert_file and args.key_file:
